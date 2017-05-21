c51980d5359e80d96d4428908b5a8e26ea0482a5
Alexander <balron923@gmail.com>
2017-05-21 21:03:33 +0300

    second commit

--- app/src/main/java/com/homework/sbt/loaderhomework/PersonListActivity.java
+++ app/src/main/java/com/homework/sbt/loaderhomework/PersonListActivity.java
@@ -1,35 +1,88 @@
 package com.homework.sbt.loaderhomework;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.homework.sbt.loaderhomework.adapter.RecyclerPersonAdapter;
+import com.homework.sbt.loaderhomework.application.PersonLoaderApplication;
+import com.homework.sbt.loaderhomework.data.Person;
+import com.homework.sbt.loaderhomework.loader.PersonLoader;
+import com.homework.sbt.loaderhomework.storage.PersonStorage;
+import com.homework.sbt.loaderhomework.storage.PersonStorageProvider;
+
+import java.util.List;
+
 public class PersonListActivity extends AppCompatActivity {
+    private static final String TAG = PersonListActivity.class.getCanonicalName();
+    private static final int LOADER_ID = 1234;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerPersonAdapter mRecyclerPersonAdapter;
+    private PersonStorage mPersonStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.e(TAG, "protected void onCreate(Bundle savedInstanceState)");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.person_list_layout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        PersonStorageProvider provider = (PersonStorageProvider) getApplication();
+        mPersonStorage = provider.getPersonStorage();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerPersonAdapter = new RecyclerPersonAdapter(mPersonStorage);
+        mRecyclerView.setAdapter(mRecyclerPersonAdapter);
+
+        getSupportLoaderManager().initLoader(LOADER_ID, null, new PersonLoaderCallbacks());
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.e(TAG, "public boolean onCreateOptionsMenu(Menu menu)");
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.e(TAG, "public boolean onOptionsItemSelected(MenuItem item)");
         int id = item.getItemId();
-
-        if (id == R.id.add_new_data) {
+        if (id == R.id.add_new_person_menu_item) {
+            startActivity(new Intent(this, AddPersonActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private class PersonLoaderCallbacks implements LoaderManager.LoaderCallbacks<List<Person>> {
+        private String TAG = PersonLoaderCallbacks.class.getCanonicalName();
+
+        @Override
+        public Loader<List<Person>> onCreateLoader(int id, Bundle args) {
+            Log.e(TAG, "public Loader<List<Person>> onCreateLoader(int id, Bundle args)");
+            return new PersonLoader(PersonListActivity.this,mPersonStorage);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<List<Person>> loader, List<Person> data) {
+            Log.e(TAG, "public void onLoadFinished(Loader<List<Person>> loader, List<Person> data)");
+            Log.e(TAG, data.toString());
+            mRecyclerPersonAdapter.setmPersons(data);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<List<Person>> loader) {
+            Log.e(TAG, "public void onLoaderReset(Loader<List<Person>> loader)");
+        }
     }
 }
--- app/src/main/java/com/homework/sbt/loaderhomework/adapter/RecyclerPersonAdapter.java
+++ app/src/main/java/com/homework/sbt/loaderhomework/adapter/RecyclerPersonAdapter.java
@@ -1,32 +1,47 @@
 package com.homework.sbt.loaderhomework.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.homework.sbt.loaderhomework.R;
 import com.homework.sbt.loaderhomework.data.Person;
+import com.homework.sbt.loaderhomework.storage.PersonStorage;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RecyclerPersonAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private final static String TAG = RecyclerPersonAdapter.class.getCanonicalName();
     private final List<Person> mPersons;
 
-    public RecyclerPersonAdapter() {
-        mPersons = new ArrayList<>();
+    public RecyclerPersonAdapter(PersonStorage personStorage) {
+        Log.e(TAG, "RecyclerPersonAdapter constructor");
+        mPersons = personStorage.getmPersonList();
+    }
+
+    public void setmPersons(List<Person> list) {
+        mPersons.clear();
+        mPersons.addAll(list);
+        notifyDataSetChanged();
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        Log.e(TAG, "public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)");
+        return new PersonViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.person_card, parent, false));
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+        Log.e(TAG, "public void onBindViewHolder(RecyclerView.ViewHolder holder, int position)");
+        Person person = mPersons.get(position);
+        ((PersonViewHolder) holder).fillTextView(person.getmFirstName(), person.getmLastName(), person.getmAge());
     }
+
 
     @Override
     public int getItemCount() {
@@ -38,11 +53,17 @@ public class RecyclerPersonAdapter extends RecyclerView.Adapter<RecyclerView.Vie
         TextView mLastNameTextView;
         TextView mAgeTextView;
 
-        public PersonViewHolder(View itemView) {
+        PersonViewHolder(View itemView) {
             super(itemView);
-//            mFirstNameTextView = itemView.findViewById(R.id.);
-//            mLastNameTextView = itemView.findViewById(R.id);
-//            mAgeTextView = itemView.findViewById(R.id);
+            mFirstNameTextView = (TextView) itemView.findViewById(R.id.person_firstname_text_view);
+            mLastNameTextView = (TextView) itemView.findViewById(R.id.person_lastname_text_view);
+            mAgeTextView = (TextView) itemView.findViewById(R.id.person_age_text_view);
+        }
+
+        void fillTextView(String firstName, String lastName, int age) {
+            mFirstNameTextView.setText(firstName);
+            mLastNameTextView.setText(lastName);
+            mAgeTextView.setText(String.valueOf(age));
         }
     }
 }
